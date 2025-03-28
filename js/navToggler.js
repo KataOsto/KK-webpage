bba5a00feeda8d23c7786d6c1c80aa2d95a5ccfe
kataosto@gmail.com <kataosto@gmail.com>
2025-03-28 18:28:59 +0100

    nav and hero edit

--- js/navToggler.js
+++ js/navToggler.js
@@ -4,3 +4,11 @@ const navList = document.querySelector('.nav__list');
 navToggler.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
